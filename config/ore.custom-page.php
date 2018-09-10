416af42e0a69d860d4802a64682dadc719693bbd
railken <railken@protonmail.ch>
2018-09-10 16:47:45 +0200

    fix prefix api

--- config/ore.custom-page.php
+++ config/ore.custom-page.php
@@ -107,7 +107,7 @@ return [
             'enabled'    => true,
             'controller' => Railken\LaraOre\Http\Controllers\Admin\CustomPagesController::class,
             'router'     => [
-                'prefix' => '/admin/custom-pages',
+                'prefix' => '/custom-pages',
             ],
         ],
     ],
